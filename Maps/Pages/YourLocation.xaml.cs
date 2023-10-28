cafe1acce96b5fd48b5a06a2607b80d49ff2d1b3
1matux1 <mateusz.kmak@onet.pl>
2023-10-28 19:31:27 +0200

    1st
    Dużo roboty :100:

---
+++ Maps/Pages/YourLocation.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Xamarin.Forms.Xaml;
+
+namespace Maps.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class YourLocation : ContentPage
+	{
+		public YourLocation ()
+		{
+			InitializeComponent ();
+
+            var initialLocation = new Position(37.7749, -122.4194); // San Francisco, CA
+            Mapa.MoveToRegion(MapSpan.FromCenterAndRadius(initialLocation, Distance.FromMiles(1.0))); // nie skompiluje, nie ma tokena API
+        }
+	}
+}
